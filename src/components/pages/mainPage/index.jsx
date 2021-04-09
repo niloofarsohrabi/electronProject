adae26883290b444d1689d19bf8c0573173b113b
niloofarssohrabi <nilufar.sohrabi@gmail.com>
2021-04-09 20:04:44 +0430

    convert to material

--- src/components/pages/mainPage/index.jsx
+++ src/components/pages/mainPage/index.jsx
@@ -1,67 +1,81 @@
-import React, { useEffect } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { AddMember } from '../../services/addMember'
-import { GroupAddedMembers } from '../../services/groupAddedMembers'
-import { Search } from '../../services/search'
-import { searchResultAction } from '../../../stateManagment/actions/searchResultAction'
-import TextField from '@material-ui/core/TextField'
-import profileStyle from '../../../styles/profileStyle.module.scss'
-import '../../../styles/mainPageGlobalStyle.scss'
-
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { AddMember } from '../../services/addMember';
+import { GroupAddedMembers } from '../../services/groupAddedMembers';
+import { Search } from '../../services/search';
+import { searchResultAction } from '../../../stateManagment/actions/searchResultAction';
+import TextField from '@material-ui/core/TextField';
+import profileStyle from '../../../styles/profileStyle.module.scss';
+import Button from '@material-ui/core/Button';
+import '../../../styles/mainPageGlobalStyle.scss';
 
 export const MainPage = () => {
+  const dispatch = useDispatch();
+  const showSearchResult = useSelector(
+    (state) => state.searchResult.searchResultState,
+  );
+  const copyContactListState = useSelector(
+    (state) => state.contactList.contactListState,
+  );
+  useEffect(() => {
+    dispatch(searchResultAction(copyContactListState));
+  }, [copyContactListState]);
 
-    const dispatch = useDispatch();
-    const showSearchResult = useSelector(state => state.searchResult.searchResultState);
-    const copyContactListState = useSelector(state => state.contactList.contactListState);
-    useEffect(() => {
-        dispatch(searchResultAction(copyContactListState));
-    }, [copyContactListState])
+  return (
+    <>
+      <div className="GroupTitleTextBox">
+        <TextField
+          id="outlined-search"
+          label="Group Title"
+          type="text"
+          variant="outlined"
+        />
+      </div>
+      <GroupAddedMembers />
+      <Search />
 
-    return (
-        <>
-            <div className="GroupTitleTextBox">
-                <TextField id="outlined-search" label="Group Title" type="text" variant="outlined" />
+      {showSearchResult.length == 0 ? (
+        <div className="notFound">
+          <p>No results Found</p>
+        </div>
+      ) : (
+        showSearchResult &&
+        showSearchResult.map((item) => {
+          return (
+            <div key={item.id} className={profileStyle.container}>
+              <div className={profileStyle.totalProfile}>
+                <div className={profileStyle.totalImg}>
+                  <img className={profileStyle.profileImg} src={item.avatar} />
+                  {item.status === 'ONLINE' ? (
+                    <div className={profileStyle.onLineStatus}></div>
+                  ) : (
+                    <div className={profileStyle.offLineStatus}></div>
+                  )}
+                </div>
+                <div className={profileStyle.styleDetail}>
+                  <div className={profileStyle.profileName}>
+                    {item.fullName}
+                  </div>
+                  <div className={profileStyle.workingSide}>
+                    {item.workingSide}
+                  </div>
+                </div>
+                <AddMember />
+              </div>
+              <hr />
             </div>
-            <GroupAddedMembers />
-            <Search />
+          );
+        })
+      )}
 
-            {
-                showSearchResult.length == 0 ?
-                    <div className="notFound">
-                        <p>No results Found</p>
-                    </div>
-                    : showSearchResult &&
-                    showSearchResult.map((item) => {
-                        return (
-                            <div key={item.id} className={profileStyle.container}>
-                                <div className={profileStyle.totalProfile}>
-                                    <div className={profileStyle.totalImg}>
-                                        <img className={profileStyle.profileImg} src={item.avatar} />
-                                        {
-                                            item.status === "ONLINE" ?
-                                                <div className={profileStyle.onLineStatus}></div> :
-                                                <div className={profileStyle.offLineStatus}></div>
-                                        }
-                                    </div>
-                                    <div className={profileStyle.styleDetail}>
-                                        <div className={profileStyle.profileName}>{item.fullName}</div>
-                                        <div className={profileStyle.workingSide}>{item.workingSide}</div>
-                                    </div>
-                                    <AddMember />
-                                </div>
-                                <hr />
-                            </div>
-                        )
-                    })
-            }
-
-            <div className="totalBtn">
-                <button className="discardBtn">Discard</button>
-                <button className="createBtn">Create</button>
-            </div>
-
-
-        </>
-    )
-}
+      <div className="totalButton">
+        <Button variant="outlined" color="primary">
+          Discard
+        </Button>
+        <Button variant="contained" color="primary">
+          Create
+        </Button>
+      </div>
+    </>
+  );
+};
--- src/components/services/search/index.jsx
+++ src/components/services/search/index.jsx
@@ -1,35 +1,46 @@
-import React, { useState, useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { searchResultAction } from '../../../stateManagment/actions/searchResultAction'
-//import SearchIcon from '@material-ui/icons/Search';
-//import searchStyle from '../../../styles/searchStyle.module.scss'
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { searchResultAction } from '../../../stateManagment/actions/searchResultAction';
+import SearchIcon from '@material-ui/icons/Search';
+import '../../../styles/searchStyle.scss';
 
 import TextField from '@material-ui/core/TextField';
+import InputAdornment from '@material-ui/core/InputAdornment';
 
 export const Search = () => {
+  const [userInputForSearch, setUserInputForSearch] = useState();
+  const showContactListState = useSelector(
+    (state) => state.contactList.contactListState,
+  );
+  const dispatch = useDispatch();
 
-    const [userInputForSearch, setUserInputForSearch] = useState();
-    const showContactListState = useSelector(state => state.contactList.contactListState);
-    const dispatch = useDispatch()
+  useEffect(() => {
+    let userResultSearch =
+      showContactListState &&
+      showContactListState.filter(
+        (item) =>
+          item.fullName.toLowerCase().includes(userInputForSearch) ||
+          item.workingSide.toLowerCase().includes(userInputForSearch),
+      );
+    dispatch(searchResultAction(userResultSearch));
+  }, [userInputForSearch]);
 
-    useEffect(() => {
-        let userResultSearch = showContactListState && showContactListState.filter(item => (item.fullName).toLowerCase().includes(userInputForSearch)
-        ||(item.workingSide).toLowerCase().includes(userInputForSearch));
-        dispatch(searchResultAction(userResultSearch));
-    }, [userInputForSearch])
-
-
-    return (
-        <>
-            {/* <div className={searchStyle.searchBox}>
-               <div className={searchStyle.searchIcon}>
-                   <SearchIcon/>
-               </div>
-               <input placeholder="Search member by name or working side"/>
-           </div> */}
-
-            <TextField id="standard-basic" label="Standard" onChange={(event) => setUserInputForSearch(event.target.value)} />
-
-        </>
-    )
-}
+  return (
+    <>
+      <div className="textFieldSearch">
+        <TextField
+          id="input-with-icon-textfield"
+          placeholder="Search Member By Name Or Working Side"
+          onChange={(event) => setUserInputForSearch(event.target.value)}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon />
+              </InputAdornment>
+            ),
+          }}
+        />
+      </div>
+    </>
+  );
+};
